ee601c5f8ff202ad65f08e33a1bf15f1d225896a
paula rosa <paula.mecatronica@gmail.com>
2023-04-15 13:00:09 +0200

    Cria tela detalhe e intent para abrir nova tela

--- app/src/main/java/com/androidmentoria/gatos/GatoActivity.kt
+++ app/src/main/java/com/androidmentoria/gatos/GatoActivity.kt
@@ -1,12 +1,20 @@
 package com.androidmentoria.gatos
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class GatoActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gato)
+
+        val button: Button = findViewById(R.id.botaDetalheGato)
+        button.setOnClickListener {
+            val intent = Intent(this, GatoDetalheActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/androidmentoria/gatos/GatoDetalheActivity.kt
@@ -0,0 +1,12 @@
+package com.androidmentoria.gatos
+
+import android.app.Activity
+import android.os.Bundle
+
+class GatoDetalheActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gato_detalhe)
+    }
+}
