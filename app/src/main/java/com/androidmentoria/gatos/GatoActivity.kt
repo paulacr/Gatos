d02f0d2a426f7522aadeb49f37f0daa161c020d3
paula rosa <paula.mecatronica@gmail.com>
2023-04-15 12:42:10 +0200

    Cria tela com gato e botão

--- app/src/main/java/com/androidmentoria/gatos/GatoActivity.kt
+++ app/src/main/java/com/androidmentoria/gatos/GatoActivity.kt
@@ -1,8 +1,12 @@
 package com.androidmentoria.gatos
 
 import android.app.Activity
+import android.os.Bundle
 
 class GatoActivity : Activity() {
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gato)
+    }
 }
